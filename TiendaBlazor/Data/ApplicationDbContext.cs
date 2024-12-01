75b4980b8795b7da359dc8d3754fea2a8861385e
Sebstian Guadalupe Tanori Ruiz <tanoriruizs@gmail.com>
2024-12-01 16:41:02 -0700

    Haciendos cambios visualmente falta corregir algunos errores

--- TiendaBlazor/Data/ApplicationDbContext.cs
+++ TiendaBlazor/Data/ApplicationDbContext.cs
@@ -19,11 +19,11 @@ namespace TiendaBlazor.Data
             // Configuración de precisión para Producto.Precio y Carrito.Total
             modelBuilder.Entity<Producto>()
                 .Property(p => p.Precio)
-                .HasPrecision(18, 4);
+                .HasPrecision(18, 2);
 
             modelBuilder.Entity<Carrito>()
                 .Property(c => c.Total)
-                .HasPrecision(18, 4);
+                .HasPrecision(18, 2);
 
             modelBuilder.Entity<Usuario>().HasData(
                 new Usuario { Id = 1, Nombre = "Admin", Email = "admin@correo.com", Contraseña = "admin123", Rol = "Admin" },
@@ -31,16 +31,16 @@ namespace TiendaBlazor.Data
             );
 
             modelBuilder.Entity<Producto>().HasData(
-                new Producto { Id = 1, Nombre = "Macbook", Descripcion = "Laptop para trabajo", Precio = 10.99m, Stock = 100, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/mac.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 2, Nombre = "Playera Polo", Descripcion = "Playera polo elegante", Precio = 20.99m, Stock = 200, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/polo.jpg"), TipoImagen = "image/jpg" },
-                new Producto { Id = 3, Nombre = "Converse", Descripcion = "Tenis comodos", Precio = 30.99m, Stock = 300, Categoria = "Calzado", Imagen = File.ReadAllBytes("wwwroot/img/productos/converse.png"), TipoImagen = "image/png" },
-                new Producto { Id = 4, Nombre = "Refigerador", Descripcion = "Refigerador moderno", Precio = 40.99m, Stock = 400, Categoria = "Linea Blanca", Imagen = File.ReadAllBytes("wwwroot/img/productos/refigerador.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 5, Nombre = "Iphone", Descripcion = "Iphone moderno", Precio = 50.99m, Stock = 500, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/iphone.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 6, Nombre = "Pantalon Levis", Descripcion = "Pantalon comodo", Precio = 60.99m, Stock = 600, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/pantalon.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 7, Nombre = "Nike", Descripcion = "Tenis comodos", Precio = 70.99m, Stock = 700, Categoria = "Calzado", Imagen = File.ReadAllBytes("wwwroot/img/productos/nike.jpg"), TipoImagen = "image/jpg" },
-                new Producto { Id = 8, Nombre = "Lavadora", Descripcion = "Lavadora moderna", Precio = 80.99m, Stock = 800, Categoria = "Linea Blanca", Imagen = File.ReadAllBytes("wwwroot/img/productos/lavadora.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 9, Nombre = "Audifonos", Descripcion = "Audifonos modernos", Precio = 90.99m, Stock = 900, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/audifonos.webp"), TipoImagen = "image/webp" },
-                new Producto { Id = 10, Nombre = "Playera Adidas", Descripcion = "PLayera comoda", Precio = 100.99m, Stock = 1000, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/playeraadidas.jpg"), TipoImagen = "image/jpg" }
+                new Producto { Id = 1, Nombre = "Macbook", Descripcion = "Laptop para trabajo", Precio = Math.Round(10.99m, 2), Stock = 100, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/mac.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 2, Nombre = "Playera Polo", Descripcion = "Playera polo elegante", Precio = Math.Round(20.99m, 2), Stock = 200, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/polo.jpg"), TipoImagen = "image/jpg" },
+                new Producto { Id = 3, Nombre = "Converse", Descripcion = "Tenis comodos", Precio = Math.Round(30.99m, 2), Stock = 300, Categoria = "Calzado", Imagen = File.ReadAllBytes("wwwroot/img/productos/converse.png"), TipoImagen = "image/png" },
+                new Producto { Id = 4, Nombre = "Refigerador", Descripcion = "Refigerador moderno", Precio = Math.Round(40.99m, 2), Stock = 400, Categoria = "Linea Blanca", Imagen = File.ReadAllBytes("wwwroot/img/productos/refigerador.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 5, Nombre = "Iphone", Descripcion = "Iphone moderno", Precio = Math.Round(50.99m, 2), Stock = 500, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/iphone.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 6, Nombre = "Pantalon Levis", Descripcion = "Pantalon comodo", Precio = Math.Round(60.99m, 2), Stock = 600, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/pantalon.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 7, Nombre = "Nike", Descripcion = "Tenis comodos", Precio = Math.Round(70.99m, 2), Stock = 700, Categoria = "Calzado", Imagen = File.ReadAllBytes("wwwroot/img/productos/nike.jpg"), TipoImagen = "image/jpg" },
+                new Producto { Id = 8, Nombre = "Lavadora", Descripcion = "Lavadora moderna", Precio = Math.Round(80.99m, 2), Stock = 800, Categoria = "Linea Blanca", Imagen = File.ReadAllBytes("wwwroot/img/productos/lavadora.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 9, Nombre = "Audifonos", Descripcion = "Audifonos modernos", Precio = Math.Round(90.99m, 2), Stock = 900, Categoria = "Electronicos", Imagen = File.ReadAllBytes("wwwroot/img/productos/audifonos.webp"), TipoImagen = "image/webp" },
+                new Producto { Id = 10, Nombre = "Playera Adidas", Descripcion = "Playera comoda", Precio = Math.Round(100.99m, 2), Stock = 1000, Categoria = "Ropa", Imagen = File.ReadAllBytes("wwwroot/img/productos/playeraadidas.jpg"), TipoImagen = "image/jpg" }
             );
         }
 
