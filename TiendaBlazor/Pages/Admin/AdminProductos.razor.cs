d679cfaf692341bce8509987a5e27b1475aca022
Sebstian Guadalupe Tanori Ruiz <tanoriruizs@gmail.com>
2024-12-02 07:00:29 -0700

    Realizando cambios visuales en la parte de la vistas y agregando nueva funcionalidad de filtrado y cambiando la opcion de buscar por nombre en vez por categoria

---
+++ TiendaBlazor/Pages/Admin/AdminProductos.razor.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using TiendaBlazor.Models;
+using TiendaBlazor.Services;
+
+
+namespace TiendaBlazor.Pages.Admin
+{
+    public partial class AdminProductos
+    {
+        private List<Producto> productos = new();
+        private List<Producto> productosFiltrados = new();
+        private string searchTerm = string.Empty;
+        private int registrosVisibles = 10; 
+        private string? UserName;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity is { IsAuthenticated: true })
+            {
+                if (user.IsInRole("Admin"))
+                {
+                    UserName = user.Identity.Name;
+
+                    productos = (await ProductoService.GetAllProductosAsync()).ToList();
+                    productosFiltrados = productos;
+                }
+                else
+                {
+                    NavigationManager.NavigateTo("/NoAutorizado");
+                }
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/Login");
+            }
+        }
+
+        private void EditarProducto(int id)
+        {
+            NavigationManager.NavigateTo($"/admin-editar/{id}");
+        }
+
+        private async Task EliminarProducto(int id)
+        {
+            bool confirmado = await JsRuntime.InvokeAsync<bool>("confirm", "¿Estás seguro de que deseas eliminar este producto?");
+            if (confirmado)
+            {
+                await ProductoService.DeleteProductoAsync(id);
+                productos = (await ProductoService.GetAllProductosAsync()).ToList();
+                FiltrarProductos();
+            }
+        }
+
+        private void FiltrarProductos()
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                productosFiltrados = productos;
+            }
+            else
+            {
+                productosFiltrados = productos
+                    .Where(p => p.Nombre.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+        }
+
+        private void Buscar(ChangeEventArgs e)
+        {
+            searchTerm = e.Value?.ToString() ?? string.Empty;
+            FiltrarProductos();
+        }
+    }
+}
